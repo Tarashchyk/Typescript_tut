fd459ee753a1c74b6db543a882e1cb753f23e398
Tarashchyk <55180864+Tarashchyk@users.noreply.github.com>
2020-02-04 16:14:09 +0200

    initialCommit

---
+++ build/precache-manifest.fbd230dd10f96500dbfeaf60eadc5719.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78e5898213bd13fc1a263e7e71f8acf2",
+    "url": "/YevhenTarashchyk/TypeScript/index.html"
+  },
+  {
+    "revision": "15f111040233b11a799a",
+    "url": "/YevhenTarashchyk/TypeScript/static/css/main.bc849963.chunk.css"
+  },
+  {
+    "revision": "3179fbf87047bbc99088",
+    "url": "/YevhenTarashchyk/TypeScript/static/js/2.9b74128a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/YevhenTarashchyk/TypeScript/static/js/2.9b74128a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "15f111040233b11a799a",
+    "url": "/YevhenTarashchyk/TypeScript/static/js/main.439abf16.chunk.js"
+  },
+  {
+    "revision": "6260cdc6a68fdcb59b14",
+    "url": "/YevhenTarashchyk/TypeScript/static/js/runtime-main.bc5e2d21.js"
+  }
+]);
